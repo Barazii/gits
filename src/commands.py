f2f0f57a4cfff14391f1a592c21cb6ea2e5fb73c
Barazii <mahmoud.baraziii@gmail.com>
2025-01-22 18:25:00 +0100

    Replaced the print statements with log statements using logging library

--- src/commands.py
+++ src/commands.py
@@ -1,11 +1,15 @@
 from src.scheduler import schedule_push, schedule_commit
+import logging
+
+
+logger = logging.getLogger("gits")
 
 
 def schedule_push_cmd(timestamp, task_complete):
     dt = schedule_push(timestamp, task_complete)
-    print(f"A push scheduled for {dt}")
+    logger.info(f"A push scheduled for {dt}")
 
 
 def schedule_commit_cmd(message, timestamp, task_complete):
     dt = schedule_commit(message, timestamp, task_complete)
-    print(f"A commit scheduled for {dt}")
+    logger.info(f"A commit scheduled for {dt}")
--- src/scheduler.py
+++ src/scheduler.py
@@ -1,6 +1,10 @@
 from threading import Timer
 from datetime import datetime
 from src.git_commands import *
+import logging
+
+
+logger = logging.getLogger("gits")
 
 
 def schedule_push(timestamp, task_complete):
@@ -10,7 +14,7 @@ def schedule_push(timestamp, task_complete):
         timer = Timer(delay, execute_push, args=[task_complete])
         timer.start()
     else:
-        print("Cannot schedule a push for the past")
+        logger.info("Cannot schedule a push for the past")
     return dt
 
 
@@ -21,17 +25,17 @@ def schedule_commit(message, timestamp, task_complete):
         timer = Timer(delay, execute_commit, args=[message, task_complete])
         timer.start()
     else:
-        print("Cannot schedule a push for the past")
+        logger.info("Cannot schedule a push for the past")
     return dt
 
 
 def execute_push(task_complete):
-    print("Executing git push")
+    logger.info("Executing git push")
     git_push()
     task_complete.set()
 
 
 def execute_commit(message, task_complete):
-    print(f'Executing git commit -m "{message}"')
+    logger.info(f'Executing git commit -m "{message}"')
     git_commit(message=message)
     task_complete.set()
