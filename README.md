2baff64d544c6d412d58ec4bf856ae83dff92afe
Barazii <mahmoud.baraziii@gmail.com>
2025-01-24 00:59:48 +0100

    Refoactored the setup/install script.

--- README.md
+++ README.md
@@ -5,8 +5,6 @@
 
 future improvement
 run the scheduler on the cloud so that the laptop can be turned off.
-remove year from timestamp.
-update setup script to install gits system-wide not only in virt env.
-make the setting up of the auto compltete like adding that line in the bashrc script included in the installing of gits in the setup script. 
 add unit testing. automate it also.
 add multiple commands. ex: many changes to be commited in multiple commit messages, so need to run gits commit 3 times after each other with 3 messages.
+upload PyPI.
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
+
+def parse_requirements(filename):
+    with open(filename, "r") as file:
+        return [line.strip() for line in file if line.strip()]
+
+
 setup(
     name="gits",
+    description="CLI program to schedule git push or commit commands for execution at a specified time",
     version="1.0",
+    author="mahmoud barazi",
     packages=find_packages(),
-    install_requires=["argcomplete"],
+    install_requires=parse_requirements("requirements.txt"),
     entry_points="""
         [console_scripts]
         gits=src.gits:gits
