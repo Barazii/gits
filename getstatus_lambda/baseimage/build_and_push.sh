f1c11ae0fe09cbb2a45f6b3f0d2b6622726c9a48
Barazii <mahmoud.baraziii@gmail.com>
2025-11-07 15:50:30 +0100

    Added base docker images to separate build stage from final image stage to speed the compile of the lambda cpp code.

---
+++ getstatus_lambda/baseimage/build_and_push.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to build and push the base image to ECR
+# Similar to deploy.sh, with variables defined at the beginning
+
+cd "$(dirname "$0")"
+ACCOUNT_ID=$(aws sts get-caller-identity --no-cli-pager --query Account --output text)
+REGION=eu-north-1
+REPO_NAME=getstatus-lambda-base
+IMAGE_URI=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME:latest
+
+# Build
+docker build -t $REPO_NAME .
+
+# Login to ECR
+aws ecr get-login-password --no-cli-pager --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
+# Create repository if it doesn't exist
+if ! aws ecr describe-repositories --no-cli-pager --repository-names $REPO_NAME --region $REGION >/dev/null 2>&1; then
+    aws ecr create-repository --no-cli-pager --repository-name $REPO_NAME --image-scanning-configuration scanOnPush=true
+fi
+
+# Tag & Push
+docker tag $REPO_NAME:latest $IMAGE_URI
+docker push $IMAGE_URI
+
+echo "Successfully pushed $IMAGE_URI"
