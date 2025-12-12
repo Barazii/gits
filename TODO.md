2ae6f1fae94d70c91458346c679c8a9d7cdc2e69
Barazii <mahmoud.baraziii@gmail.com>
2025-12-12 04:48:28 +0100

    automate cf templates deployment

--- TODO.md
+++ TODO.md
@@ -1,8 +1,8 @@
 update readme file.
 
-stress test
+user tests
 
-discuss deployment to users on their machines. installation on their machines. deployment on aws multiple regions. subscribed users can schedule jobs, cancelled-subscription can't schedule. type basic subscription can schedule 15 jobs, pro sub can sched unlimited jobs.
+discuss subscribed users and cancelled-subscription accessibility.
 
 make check for git repo and config variables defined at begining of program run. gits.cpp: duplicate check of current git repo.
 
@@ -13,11 +13,6 @@ discuss schedule local time again.    <<<-------
 test on macOS
 
 add support to gitlab.
-
-delete all eu-north-1 resources -> delete eu-west-3 resources -> deploy eveything on eu-north-1 back again.
-make bash file to delpoy all templates together with permission to do that without errors.
-
-combine the 2 github action files. 
 
 discuss vps for the whole cloud resources for optimised customised security
 
--- cloudformation/deploy-all.sh
+++ cloudformation/deploy-all.sh
@@ -5,13 +5,18 @@ set -e
 # Configuration
 PROJECT_NAME="gits"
 REGION="eu-west-3"
-GITHUB_TOKEN=$(grep '^GITHUB_TOKEN=' ../.gits/config | cut -d'=' -f2-)
+GITHUB_TOKEN=$(grep '^GITHUB_TOKEN=' ~/.gits/config | cut -d'=' -f2-)
+
+# Store original credentials to allow re-assuming the role
+ORIGINAL_AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-}"
+ORIGINAL_AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-}"
+ORIGINAL_AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN:-}"
 
 # Deploy IAM stack first with current credentials
 echo "Deploying IAM stack first..."
 aws cloudformation deploy \
     --stack-name "gits-iam" \
-    --template-file "cloudformation/iam.yaml" \
+    --template-file "iam.yaml" \
     --region "$REGION" \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides ProjectName=$PROJECT_NAME DynamoTableName=${PROJECT_NAME}-jobs ArtifactBucketName=${PROJECT_NAME}-artifacts
@@ -21,14 +26,29 @@ echo "IAM stack deployed successfully."
 ROLE_ARN=$(aws cloudformation describe-stacks --stack-name "gits-iam" --query 'Stacks[0].Outputs[?OutputKey==`CloudFormationDeploymentRoleArn`].OutputValue' --output text --region "$REGION")
 echo "Retrieved role ARN: $ROLE_ARN"
 
-# Assume the CloudFormation deployment role
-echo "Assuming CloudFormation deployment role..."
-ASSUME_ROLE_OUTPUT=$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "deployment")
-export AWS_ACCESS_KEY_ID=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.AccessKeyId')
-export AWS_SECRET_ACCESS_KEY=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SecretAccessKey')
-export AWS_SESSION_TOKEN=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SessionToken')
+# Function to assume the deployment role (can be called multiple times to refresh)
+assume_deployment_role() {
+    echo "Assuming CloudFormation deployment role..."
+    # Temporarily restore original credentials to assume role
+    if [ -n "$ORIGINAL_AWS_ACCESS_KEY_ID" ]; then
+        export AWS_ACCESS_KEY_ID="$ORIGINAL_AWS_ACCESS_KEY_ID"
+        export AWS_SECRET_ACCESS_KEY="$ORIGINAL_AWS_SECRET_ACCESS_KEY"
+        export AWS_SESSION_TOKEN="$ORIGINAL_AWS_SESSION_TOKEN"
+    else
+        unset AWS_ACCESS_KEY_ID
+        unset AWS_SECRET_ACCESS_KEY
+        unset AWS_SESSION_TOKEN
+    fi
+    
+    ASSUME_ROLE_OUTPUT=$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "deployment")
+    export AWS_ACCESS_KEY_ID=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.AccessKeyId')
+    export AWS_SECRET_ACCESS_KEY=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SecretAccessKey')
+    export AWS_SESSION_TOKEN=$(echo $ASSUME_ROLE_OUTPUT | jq -r '.Credentials.SessionToken')
+    echo "Role assumed successfully."
+}
 
-echo "Role assumed successfully."
+# Initial role assumption
+assume_deployment_role
 
 # Function to deploy a stack
 deploy_stack() {
@@ -48,39 +68,51 @@ deploy_stack() {
 }
 
 # Deploy remaining stacks in order
-deploy_stack "gits-ecr" "cloudformation/ecr.yaml" "" "ProjectName=$PROJECT_NAME ImageTagMutability=MUTABLE ScanOnPush=true EncryptionType=AES256"
+deploy_stack "gits-ecr" "ecr.yaml" "" "ProjectName=$PROJECT_NAME ImageTagMutability=MUTABLE ScanOnPush=true EncryptionType=AES256"
 
 echo "Building and pushing Lambda images..."
+
+# Refresh credentials before each Lambda build (base image builds take ~8-9 minutes each)
+assume_deployment_role
 ../gitsops_lambda/baseimage/build_and_push.sh
 ../gitsops_lambda/deploy.sh
 REPO_URI_SCHEDULE=$(aws ecr describe-repositories --repository-names gits-schedule-lambda --query 'repositories[0].repositoryUri' --output text --region $REGION)
 IMAGE_URI_SCHEDULE=$REPO_URI_SCHEDULE@$(aws ecr describe-images --repository-name gits-schedule-lambda --query 'imageDetails[0].imageDigest' --output text --region $REGION)
+
+assume_deployment_role
 ../delete_lambda/baseimage/build_and_push.sh
 ../delete_lambda/deploy.sh
 REPO_URI_DELETE=$(aws ecr describe-repositories --repository-names gits-delete-lambda --query 'repositories[0].repositoryUri' --output text --region $REGION)
 IMAGE_URI_DELETE=$REPO_URI_DELETE@$(aws ecr describe-images --repository-name gits-delete-lambda --query 'imageDetails[0].imageDigest' --output text --region $REGION)
+
+assume_deployment_role
 ../getstatus_lambda/baseimage/build_and_push.sh
 ../getstatus_lambda/deploy.sh
 REPO_URI_STATUS=$(aws ecr describe-repositories --repository-names gits-status-lambda --query 'repositories[0].repositoryUri' --output text --region $REGION)
 IMAGE_URI_STATUS=$REPO_URI_STATUS@$(aws ecr describe-images --repository-name gits-status-lambda --query 'imageDetails[0].imageDigest' --output text --region $REGION)
+
+assume_deployment_role
 ../codebuildlense_lambda/baseimage/build_and_push.sh
 ../codebuildlense_lambda/deploy.sh
 REPO_URI_CODEBUILD_LENS=$(aws ecr describe-repositories --repository-names gits-codebuildlens-lambda --query 'repositories[0].repositoryUri' --output text --region $REGION)
 IMAGE_URI_CODEBUILD_LENS=$REPO_URI_CODEBUILD_LENS@$(aws ecr describe-images --repository-name gits-codebuildlens-lambda --query 'imageDetails[0].imageDigest' --output text --region $REGION)
 
-deploy_stack "gits-s3" "cloudformation/s3.yaml" "" "BucketName=${PROJECT_NAME}-artifacts EnableVersioning=true BlockPublicAccess=true RetainOnDelete=false"
+# Refresh credentials before deploying remaining stacks
+assume_deployment_role
 
-deploy_stack "gits-dynamodb" "cloudformation/dynamodb.yaml" "" "TableName=${PROJECT_NAME}-jobs PointInTimeRecovery=ENABLED BillingMode=PAY_PER_REQUEST"
+deploy_stack "gits-s3" "s3.yaml" "" "BucketName=${PROJECT_NAME}-artifacts EnableVersioning=true BlockPublicAccess=true RetainOnDelete=false"
 
-deploy_stack "gits-secret-manager" "cloudformation/secretmanager.yaml" "" "ProjectName=$PROJECT_NAME GitHubToken=$GITHUB_TOKEN"
+deploy_stack "gits-dynamodb" "dynamodb.yaml" "" "TableName=${PROJECT_NAME}-jobs PointInTimeRecovery=ENABLED BillingMode=PAY_PER_REQUEST"
 
-deploy_stack "gits-codebuild" "cloudformation/codebuild.yaml" "" "ProjectName=$PROJECT_NAME ArtifactBucketName=${PROJECT_NAME}-artifacts"
+deploy_stack "gits-secret-manager" "secretmanager.yaml" "" "ProjectName=$PROJECT_NAME GitHubToken=$GITHUB_TOKEN"
 
-deploy_stack "gits-lambdas" "cloudformation/lambdas.yaml" "--capabilities CAPABILITY_IAM" "ProjectName=$PROJECT_NAME DynamoTableName=${PROJECT_NAME}-jobs ArtifactBucketName=${PROJECT_NAME}-artifacts CodeBuildProjectName=$PROJECT_NAME ImageUriSchedule=$IMAGE_URI_SCHEDULE ImageUriDelete=$IMAGE_URI_DELETE ImageUriStatus=$IMAGE_URI_STATUS ImageUriCodeBuildLens=$IMAGE_URI_CODEBUILD_LENS"
+deploy_stack "gits-codebuild" "codebuild.yaml" "" "ProjectName=$PROJECT_NAME ArtifactBucketName=${PROJECT_NAME}-artifacts"
 
-deploy_stack "gits-events" "cloudformation/eventbridge.yaml" "" "ProjectName=$PROJECT_NAME"
+deploy_stack "gits-lambdas" "lambdas.yaml" "--capabilities CAPABILITY_IAM" "ProjectName=$PROJECT_NAME DynamoTableName=${PROJECT_NAME}-jobs ArtifactBucketName=${PROJECT_NAME}-artifacts CodeBuildProjectName=$PROJECT_NAME ImageUriSchedule=$IMAGE_URI_SCHEDULE ImageUriDelete=$IMAGE_URI_DELETE ImageUriStatus=$IMAGE_URI_STATUS ImageUriCodeBuildLens=$IMAGE_URI_CODEBUILD_LENS"
 
-deploy_stack "gits-apigateway" "cloudformation/apigateway.yaml" "" "ProjectName=$PROJECT_NAME"
+deploy_stack "gits-events" "eventbridge.yaml" "" "ProjectName=$PROJECT_NAME"
+
+deploy_stack "gits-apigateway" "apigateway.yaml" "" "ProjectName=$PROJECT_NAME"
 
 echo "All stacks deployed successfully!"
 
